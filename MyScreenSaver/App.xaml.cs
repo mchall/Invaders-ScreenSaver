794c5df70591740742ee00ab0ba031ffee4d4147
Mike <mcsyko@gmail.com>
2015-05-27 15:56:10 +0200

    Port to GitHub

---
+++ MyScreenSaver/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace MyScreenSaver
+{
+    public partial class App : Application
+    {
+        private HwndSource _winWPFContent;
+        private ScreenSaver _winSaver;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0)
+            {
+                ScreenSaver win = new ScreenSaver();
+                win.WindowState = WindowState.Maximized;
+                win.Show();
+                return;
+            }
+
+            if (e.Args[0].ToLower().StartsWith("/p"))
+            {
+                _winSaver = new ScreenSaver();
+
+                Int32 previewHandle = Convert.ToInt32(e.Args[1]);
+                IntPtr pPreviewHnd = new IntPtr(previewHandle);
+
+                RECT lpRect = new RECT();
+                bool bGetRect = Win32API.GetClientRect(pPreviewHnd, ref lpRect);
+
+                HwndSourceParameters sourceParams = new HwndSourceParameters("sourceParams");
+
+                sourceParams.PositionX = 0;
+                sourceParams.PositionY = 0;
+                sourceParams.Height = lpRect.Bottom - lpRect.Top;
+                sourceParams.Width = lpRect.Right - lpRect.Left;
+                sourceParams.ParentWindow = pPreviewHnd;
+                sourceParams.WindowStyle = (int)(WindowStyles.WS_VISIBLE | WindowStyles.WS_CHILD | WindowStyles.WS_CLIPCHILDREN);
+
+                _winWPFContent = new HwndSource(sourceParams);
+                _winWPFContent.Disposed += new EventHandler(WinWPFContent_Disposed);
+                _winWPFContent.RootVisual = _winSaver.LayoutRoot;
+            }
+            else if (e.Args[0].ToLower().StartsWith("/s"))
+            {
+                ScreenSaver win = new ScreenSaver();
+                win.WindowState = WindowState.Maximized;
+                win.Show();
+            }
+            else if (e.Args[0].ToLower().StartsWith("/c"))
+            {
+                MessageBox.Show("No settings to configure");
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void WinWPFContent_Disposed(object sender, EventArgs e)
+        {
+            _winSaver.Close();
+        }
+    }
+}
---
+++ MyScreenSaver/ScreenSaver.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace MyScreenSaver
+{
+    public partial class ScreenSaver : Window
+    {
+        private List<TextBlock> _spaceInvaderTextBlocks;
+
+        private Point _mousePosition;
+        private bool _isActive;
+        private bool _goingDown;
+        private bool _tick;
+
+        private const double VerticalAnimationSpeed = 0.5;
+        private const double HorizontalAnimationSpeed = 10;
+
+        public ScreenSaver()
+        {
+            InitializeComponent();
+            _spaceInvaderTextBlocks = new List<TextBlock>()
+            {
+                txtSpaceInvaders1,
+                txtSpaceInvaders2,
+                txtSpaceInvaders3,
+                txtSpaceInvaders4,
+                txtSpaceInvaders5
+            };
+
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(0.5), DispatcherPriority.Normal, TimerTick, this.Dispatcher);
+
+            this.Loaded += (s, e) =>
+                {
+                    warningText.Text = "Beware\nthe\nInvasion";
+                    _goingDown = true;
+                    MoveRightAnimation();
+                };
+        }
+
+        #region Event Handlers
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _tick = !_tick;
+
+            txtSpaceInvaders1.Text = _tick ? "FFFFFFFFFFFFFFFF" : "EEEEEEEEEEEEEEEE";
+            txtSpaceInvaders2.Text = _tick ? "DDDDDDDDDDDD" : "CCCCCCCCCCCC";
+            txtSpaceInvaders3.Text = _tick ? "DDDDDDDDDDDD" : "CCCCCCCCCCCC";
+            txtSpaceInvaders4.Text = _tick ? "BBBBBBBBBBB" : "AAAAAAAAAAA";
+            txtSpaceInvaders5.Text = _tick ? "BBBBBBBBBBB" : "AAAAAAAAAAA";
+        }
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Point currentPosition = e.MouseDevice.GetPosition(this);
+            if (!_isActive)
+            {
+                _mousePosition = currentPosition;
+                _isActive = true;
+            }
+            else
+            {
+                if ((Math.Abs(_mousePosition.X - currentPosition.X) > 10) || (Math.Abs(_mousePosition.Y - currentPosition.Y) > 10))
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion Event Handlers
+
+        #region Helper Methods
+
+        private void MoveRightAnimation()
+        {
+            HorizontalAnimation(0, SystemParameters.PrimaryScreenWidth - txtSpaceInvaders1.ActualWidth, false);
+        }
+
+        private void MoveLeftAnimation()
+        {
+            HorizontalAnimation(SystemParameters.PrimaryScreenWidth - txtSpaceInvaders1.ActualWidth, 0, true);
+        }
+
+        private void HorizontalAnimation(double from, double to, bool right)
+        {
+            DoubleAnimation animation = new DoubleAnimation(from, to, TimeSpan.FromSeconds(HorizontalAnimationSpeed));
+            animation.Completed += (s, e) =>
+            {
+                CheckUpOrDown();
+                if (_goingDown)
+                    MoveDownAnimation(right);
+                else
+                    MoveUpAnimation(right);
+            };
+
+            _spaceInvaderTextBlocks.ForEach(b => b.RenderTransform.BeginAnimation(TranslateTransform.XProperty, animation));
+        }
+
+        private void MoveDownAnimation(bool right)
+        {
+            VerticalAnimation(false, right);
+        }
+
+        private void MoveUpAnimation(bool right)
+        {
+            VerticalAnimation(true, right);
+        }
+
+        private void VerticalAnimation(bool up, bool right)
+        {
+            DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(VerticalAnimationSpeed));
+            animation.Completed += (s, e) =>
+            {
+                if (right)
+                    MoveRightAnimation();
+                else
+                    MoveLeftAnimation();
+            };
+
+            foreach (var block in _spaceInvaderTextBlocks)
+            {
+                var yProp = (double)block.RenderTransform.GetValue(TranslateTransform.YProperty);
+                animation.To = yProp + (up ? -55 : 55);
+                block.RenderTransform.BeginAnimation(TranslateTransform.YProperty, animation);
+            }
+        }
+
+        private void CheckUpOrDown()
+        {
+            if (txtSpaceInvaders5.RenderTransform.Value.OffsetY >= (SystemParameters.PrimaryScreenHeight - 700))
+            {
+                _goingDown = false;
+            }
+            else if (txtSpaceInvaders1.RenderTransform.Value.OffsetY <= (100))
+            {
+                _goingDown = true;
+            }
+        }
+
+        #endregion Helper Methods
+    }
+}
